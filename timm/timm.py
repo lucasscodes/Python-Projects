eea790035d041f79f9663bd03d150d5e2a73af94
Lucas Schmidt <lucas.schmidt-1@studium.uni-hamburg.de>
2022-11-24 20:33:28 +0100

    small change

--- timm/timm.py
+++ timm/timm.py
@@ -1,7 +1,9 @@
+
 #https://timm.fast.ai/training_scripts
 
+#https://github.com/rwightman/pytorch-image-models
 
-""" Which training script?
+""" Which training script? ResNet50
     EfficientNet-B2 with RandAugment - 80.4 top-1, 95.1 top-5
     MixNet-XL with RandAugment - 80.5 top-1, 94.9 top-5
     SE-ResNeXt-26-D and SE-ResNeXt-26-T
@@ -12,3 +14,4 @@
     MobileNetV3-Large-100 - 75.766 top-1, 92,542 top-5
     ResNeXt-50 32x4d w/ RandAugment - 79.762 top-1, 94.60 top-5
     ResNet50 with JSD loss and RandAugment (clean + 2x RA augs) - 79.04 top-1, 94.39 top-5 """
+
